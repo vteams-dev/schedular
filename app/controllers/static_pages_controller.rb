73050543259f584e1c885bbdbf085d42a8c8ebd5
AdilZahid <Adil.Zahid@ucp.edu.pk>
2019-07-05 14:28:23 +0500

    minor changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
    #skip_before_action :authenticate_user!, only: [:home]
    before_action :authenticate_user!
   def home
-     @calender = ''  #current_user.calenders.build
+     @calender = current_user.calenders.build
   end
 end
