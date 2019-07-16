3fc48b8d17bd135591464097b0ddea8b0c7710a5
Faizan <faizanpervaizkhan@gmail.com>
2019-07-16 14:52:20 +0500

    Fullcalendar views implemented

--- app/javascript/calendars.js
+++ app/javascript/calendars.js
@@ -1,21 +1,55 @@
 import { Calendar } from '@fullcalendar/core';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
     var calendar = new Calendar(calendarEl, {
-        plugins: [ dayGridPlugin, interactionPlugin ],
-        dateClick: function() {
-            alert('a day has been clicked!');
+        plugins: [ timeGridPlugin,dayGridPlugin, interactionPlugin ],
+
+        header: {
+            center: 'dayGridMonth,timeGridSevenDay,dayGridFourDay,dayGridOneDay' // buttons for switching between views
         },
-        dateClick: function(info) {
-            //alert('Clicked on: ' + info.dateStr);
-            //alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-            //alert('Current view: ' + info.view.type);
-            // change the day's background color just for fun
-            info.dayEl.style.backgroundColor = 'yellow';
-        }
-    });
+
+        views: {
+            dayGridOneDay: {
+                type: 'dayGrid',
+                duration: { days: 1 },
+                buttonText: 'Today'
+            },
+            dayGridFourDay: {
+                type: 'dayGrid',
+                duration: { days: 4 },
+                buttonText: '4 day'
+            },
+
+            dayGridMonth: {
+                buttonText: 'Month'
+            },
+
+            timeGridSevenDay: {
+                type: 'timeGrid',
+                duration: { days: 7 },
+                buttonText: 'Week'
+            }
+
+        },
+
+
+        events: [
+            {
+                id: '1',
+                title: 'my event',
+                start: '2019-07-01'
+            }
+        ]
+});
+
+    var event = calendar.getEventById('1') // an event object!
+    var start = event.start // a property (a Date object)
+    console.log(start.toISOString()) // "2018-09-01T00:00:00.000Z"
+
     calendar.render();
 });
